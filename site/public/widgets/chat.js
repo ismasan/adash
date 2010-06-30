1a34ea308221ea531c560fa29677e920fd8880af
Ismael Celis <ismaelct@gmail.com>
2010-06-30 15:56:24 +0100

    Chat widget

---
+++ site/public/widgets/chat.js
@@ -0,0 +1,35 @@
+Widgets.ChatWidget = (function(){
+  
+  var server, container, list;
+  
+  function update(evt, evt_name, data){
+    list.prepend('<li><strong>'+data.info+':</strong> <span>'+data.message+'</span></li>');
+  };
+  
+  function addInputs(){
+    var form = $('<form id="chat_form"><input type="text" name="user" placeholder="User name" /><input type="text" name="message" placeholder="message" /></form>');
+    container.append(form);
+    form.submit(function(){
+      $(server).trigger('chat_message', ['chat_message', {
+        info: form.find('input[name=user]').val(),
+        message: form.find('input[name=message]').val()
+      }]);
+      form.find('input[name=message]').val('').focus();
+      return false;
+    })
+  };
+  
+  var klass = function(event_stream, container_element){
+    server = event_stream;
+    container = $('#'+container_element);
+    list = container.find('ul.messages');
+    addInputs();
+    server.bind('chat_message', update);
+  };
+  
+  klass.prototype = {
+    
+  };
+  
+  return klass;
+})();
