4fd4d169ab1548a5dd68fd0a09dc1aa75302e747
Ismael Celis <ismaelct@gmail.com>
2010-07-01 15:55:53 +0100

    Added StubEventsDispatcher

--- site/public/js/app.js
+++ site/public/js/app.js
@@ -9,6 +9,7 @@ var Utils = {
     return y+'/'+m+'/'+d+' '+h+':'+m;
   }
 }
+
 /* Ismael Celis 2010
 Simplified WebSocket events dispatcher (no channels, no users)
 
@@ -22,25 +23,47 @@ socket.bind('some_event', function(data){
 // broadcast events to all connected users
 socket.trigger( 'some_event', {name: 'ismael', message : 'Hello world'} );
 */
-
-var ServerEventsDispatcher = function(url){
-  var conn, url = url, running = false;
+var AbstractEventsDispatcher = function(){};
+AbstractEventsDispatcher.prototype = {
+  callbacks: {},
+  global_callbacks: [],
+  running: false,
   
-  var callbacks = {};
-  var global_callbacks = [];
+  isRunning: function(){return this.running},
   
-  this.bind = function(event_name, callback){
-    callbacks[event_name] = callbacks[event_name] || [];
-    callbacks[event_name].push(callback);
+  bind: function(event_name, callback){
+    this.callbacks[event_name] = this.callbacks[event_name] || [];
+    this.callbacks[event_name].push(callback);
     return this;// chainable
-  };
+  },
   
-  this.bind_all = function(callback){
-    global_callbacks.push(callback);
+  bind_all: function(callback){
+    this.global_callbacks.push(callback);
     return this;
-  };
+  },
   
-  this.trigger = function(event_name, data){
+  dispatch: function(event_name, data){
+    var chain = this.callbacks[event_name];
+    if(typeof chain == 'undefined') return; // no callbacks for this event
+    for(var i = 0; i < chain.length; i++){
+      chain[i]( data )
+    }
+  },
+  
+  dispatch_global: function(event_name, data){
+    for(var i = 0; i < this.global_callbacks.length; i++){
+      this.global_callbacks[i]( event_name, data )
+    }
+  }
+  
+};
+
+/* Wrap websocket connection
+--------------------------------------------*/
+var ServerEventsDispatcher = function(url){
+  var conn, url = url, running = false, self = this;
+  
+  this.send = function(event_name, data){
     var payload = JSON.stringify({event:event_name, data: data});
     conn.send( payload ); // <= send JSON data to socket server
     return this;
@@ -48,44 +71,64 @@ var ServerEventsDispatcher = function(url){
   
   this.stop = function(){
     conn.close();
-    running = false;
+    this.running = false;
   };
   
   this.play = function(){
+    // Create websocket
     conn = new WebSocket(url);
     // dispatch to the right handlers
     conn.onmessage = function(evt){
       try{      
         var json = JSON.parse(evt.data);
-        dispatch(json.event, json.data);
-        dispatch_global(json.event, json.data);
+        self.dispatch(json.event, json.data);
+        self.dispatch_global(json.event, json.data);
       }catch(e){
         console.log("ERROR: "+e + ' ' + evt.data)
       }
     };
 
-    conn.onclose = function(){dispatch('close',null)}
-    conn.onopen = function(){dispatch('open',null)}
-    running = true;
-  };
-  
-  this.isRunning = function(){
-    return running;
+    conn.onclose  = function(){self.dispatch('close',null)}
+    conn.onopen   = function(){self.dispatch('open',null)}
+    this.running = true;
   };
   
   this.play();
-  
-  var dispatch = function(event_name, data){
-    var chain = callbacks[event_name];
-    if(typeof chain == 'undefined') return; // no callbacks for this event
-    for(var i = 0; i < chain.length; i++){
-      chain[i]( data )
-    }
-  }
-  
-  var dispatch_global = function(event_name, data){
-    for(var i = 0; i < global_callbacks.length; i++){
-      global_callbacks[i]( event_name, data )
-    }
-  }
 };
+ServerEventsDispatcher.prototype = new AbstractEventsDispatcher;
+
+/* Periodically send out random events 
+--------------------------------------------*/
+var StubEventsDispatcher = function(){
+  
+  this.send = function(event_name, data){
+    this.dispatch(event_name, data);
+    this.dispatch_global(event_name, data);
+    return this;
+  };
+  
+  var self = this, interval;
+  
+  this.play = function(){
+    this.running = true;
+    interval = setInterval(randomEvent, 2000);
+    this.send('open', {info:'server running', event_date:Utils.now()});
+  };
+  
+  this.stop = function(){
+    this.running = false;
+    clearInterval(interval);
+    this.send('close', {info:'server stopped', event_date:Utils.now()});
+  };
+  
+  function randomEvent(){
+    self.send('order_closed', {
+      info: 'Mock order, 3 products',
+      total: 100,
+      event_date: Utils.now()
+    });
+  }
+  
+  this.play();
+};
+StubEventsDispatcher.prototype = new AbstractEventsDispatcher;
