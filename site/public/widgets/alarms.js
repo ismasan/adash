9bd2184e9ef7e60e5ec682f59a560e3bff0b0729
Ismael Celis <ismaelct@gmail.com>
2010-07-01 17:52:42 +0100

    Added audio alarms for user connectioins and chat messages

--- site/public/widgets/alarms.js
+++ site/public/widgets/alarms.js
@@ -2,7 +2,9 @@ Widgets.AlarmsWidget = (function(){
   
   var server, container;
   
-  var $audio = $('<audio src="/happy.mp3" id="order_alarm" preload="auto">No sound supported</audio>');
+  var audio =  '<audio src="/audio/order.mp3" id="order_alarm" preload="auto">No sound</audio>';
+      audio += '<audio src="/audio/message.mp3" id="message_alarm" preload="auto">No sound</audio>';
+      audio += '<audio src="/audio/connection.mp3" id="connection_alarm" preload="auto">No sound</audio>';
   
   function loadAndPlay(audio_id){
     var sound = document.getElementById(audio_id);
@@ -14,9 +16,11 @@ Widgets.AlarmsWidget = (function(){
   var klass = function(event_stream, container_element){
     server = event_stream;
     container = $('body');
-    container.append($audio);
+    container.append(audio);
     // Bind to server events
     server.bind('order_closed', function(){loadAndPlay('order_alarm')});
+    server.bind('chat_message', function(){loadAndPlay('message_alarm')});
+    server.bind('user_connected', function(){loadAndPlay('connection_alarm')});
   };
   
   return klass;
